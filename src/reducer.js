8d137f9edbd3fd8ab2580de371d794afa616a6d4
aselwilliams <aselwilliams22@gmail.com>
2022-06-24 19:16:10 -0400

    all functions exist

--- src/reducer.js
+++ src/reducer.js
@@ -18,17 +18,36 @@ const reducer=(state, action)=>{
             return {
                 ...state, cart:tempCart
             }
-            // case "DECREASE":
-            //     let tempCart=state.cart.map((cartItem)=>{
-            //         if(cartItem.id===action.payload){
-            //             return{...cartItem, amount:cartItem.amount-1}
-            //         }
-            //         return cartItem
-            //     })
-            //     return {
-            //         ...state, cart:tempCart
-            //     }
+        case "DECREASE":
+                let tCart=state.cart.map((cartItem)=>{
+                    if(cartItem.id===action.payload){
+                        return{...cartItem, amount:cartItem.amount-1}
+                    }
+                    return cartItem
+                }).filter((cartItem)=>cartItem.amount!==0)
+                return {
+                    ...state, cart:tCart
+                }
+        case 'GET_TOTALS':
+            let {total, amount}=state.cart.reduce((cartTotal, cartItem)=>{
+                const {price, amount}=cartItem
+                const itemTotal=price*amount;
+
+                cartTotal.total+=itemTotal
+                cartTotal.amount+=amount
+                return cartTotal
+            },
+            {total:0,amount:0})
+
+            total=parseFloat(total.toFixed(2))
+          return {...state,total, amount}  
+        case 'LOADING':
+            return {...state, loading:true}
+        case 'DISPLAY_ITEMS':
+            return {...state, cart: action.payload, loading:false}
+            default:
+        return state
     }
-    return state
+  
 }
 export default reducer
